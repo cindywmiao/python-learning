a941f367d01b4c295a40fba1625f9720d9532f9a
cwang <cwang@coupang.com>
2016-11-09 15:48:32 +0800

    add flask

---
+++ logging-tests/logging-practice-in-python/my_module.py
@@ -0,0 +1,22 @@
+import logging
+
+def foo():
+    logger = logging.getLogger(__name__)
+    logger.info('Hi, foo')
+
+class Bar(object):
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+
+    def bar(self):
+        self.logger.info('Hi, bar')
+
+
+
+# import logging
+# logger = logging.getLogger(__name__)
+# def foo():
+#     logger.info('Hi, foo')
+# class Bar(object):
+#     def bar(self):
+#         logger.info('Hi, bar')
